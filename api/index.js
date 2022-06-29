80a1c91467f28ca0a0559b76104493fe99217ba2
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-06-29 17:56:48 -0300

    refactor: move routes methods to /routes/index.js

--- api/index.js
+++ api/index.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const port = process.env.PORT || 3000;
+const routes = require("../api/routes");
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.json());
-
-app.get("/", (req, res) => res.status(200).send({ message: "Hello World" }));
+routes(app);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
