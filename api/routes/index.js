347793fb4be1ae4e1b23459e4e70efdd631eb242
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-07-01 18:38:52 -0300

    refactor: add remaining routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const pessoas = require("./pessoasRoute.js");
+const turmas = require("./turmasRoute.js");
+const niveis = require("./niveisRoute.js");
 
 module.exports = (app) => {
-    app.use(express.json());
-    app.use(pessoas);
+    app.use(
+    express.json(), 
+    pessoas, 
+    turmas, 
+    niveis
+    );
 };
