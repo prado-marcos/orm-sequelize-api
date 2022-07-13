9eb1e699d1f9cb71a660c60f356305a0ec9384be
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-07-13 09:37:41 -0300

    refactor: separete matriculas route from pessoasRoute

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,30 +4,17 @@ const PessoaController = require("../controllers/PessoaController.js");
 const routes = Router();
 
 routes
-    .get("/pessoas", PessoaController.listarPessoasAtivas)
-    .get("/pessoas/all", PessoaController.listarPessoas)
+    .get("/pessoas", PessoaController.listarPessoas)
+    .get("/pessoas/ativas", PessoaController.listarPessoasAtivas)
     .get("/pessoas/:id", PessoaController.acessarPessoaPorId)
-    .get(
-        "/pessoas/:pessoaId/matriculas/:matriculaId",
-        PessoaController.acessarMatricula
-    )
-    .get("/pessoas/:id/matriculas/", PessoaController.listarMatriculas)
+    .get("/pessoas/:id/matriculas/", PessoaController.listarMatriculasDePessoa)
     .post("/pessoas", PessoaController.criarPessoa)
-    .post("/pessoas/:pessoaId/matriculas/", PessoaController.criarMatricula)
     .post("/pessoas/:id/restaurar", PessoaController.restaurarPessoa)
-    .post(
-        "/pessoas/:pessoaId/matriculas/:matriculaId/restaurar",
-        PessoaController.restaurarMatricula
-    )
     .put("/pessoas/:id", PessoaController.atualizarPessoa)
-    .put(
-        "/pessoas/:pessoaId/matriculas/:matriculaId",
-        PessoaController.atualizarMatricula
+    .post(
+        "/pessoas/:estudanteId/matriculas/cancela",
+        PessoaController.cancelarMatriculas
     )
-    .delete("/pessoas/:id", PessoaController.excluirPessoa)
-    .delete(
-        "/pessoas/:pessoaId/matriculas/:matriculaId",
-        PessoaController.excluirMatricula
-    );
+    .delete("/pessoas/:id", PessoaController.excluirPessoa);
 
 module.exports = routes;
