2bbae7d7177219b32275b29cb4de6a71306567e9
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-07-13 09:35:43 -0300

    refactor: add matriculas to routes index

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const pessoas = require("./pessoasRoute.js");
 const turmas = require("./turmasRoute.js");
 const niveis = require("./niveisRoute.js");
+const matriculas = require("./matriculasRoute.js");
 
 module.exports = (app) => {
     app.use(
     express.json(), 
     pessoas, 
     turmas, 
-    niveis
+    matriculas,
+    niveis,
     );
 };
